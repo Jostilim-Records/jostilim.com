9f0fe079f93bf8830a2afbdbb32cb19e53d92569
= <=>
2026-01-09 11:43:58 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, fenix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { pkgs, system, ... }:
+      let 
+        f = with fenix.packages.${system}; combine [
+          default.toolchain
+          targets.wasm32-unknown-unknown.latest.rust-std
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            f
+            trunk
+            leptosfmt
+          ];
+        };
+      };
+    };
+}
