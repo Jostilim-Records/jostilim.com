44a133e84cd693f7d0fb6507de5fa210a8678599
= <=>
2026-01-11 11:44:53 +0100

    feat: add header

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
-use leptos::prelude::*;
+use leptos::{prelude::*};
 
 fn main() {
-    leptos::mount::mount_to_body(|| view!{ <h1>"Hello World!"</h1> })
+    leptos::mount::mount_to_body(App)
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let pages = (vec!["Home", "About", "Shop"]).iter().map(|s| s.to_string()).collect();;
+    view! {
+        <Header pages=pages></Header>
+    }
+}
+
+#[component]
+fn Header(pages: Vec<String>) -> impl IntoView {
+    view! {
+        <header>
+            <nav>
+                <ul>
+                    {pages.into_iter()
+                        .map(|p| view!{ <a href="{p}"><li>{p}</li></a> })
+                        .collect::<Vec<_>>()}
+                </ul>
+            </nav>
+        </header>
+    }
 }
