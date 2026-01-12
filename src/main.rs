bf86adce8dd5b5bdad665caa85b7cef81d499d9b
= <=>
2026-01-12 12:05:23 +0100

    feat: dynamic list of buttons

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::net::Ipv6Addr;
+
 use leptos::{prelude::*};
 
 fn main() {
@@ -25,6 +27,7 @@ fn Header(pages: Vec<String>) -> impl IntoView {
             </nav>
         </header>
         <StaticList length=5></StaticList>
+        <DynamicList initial_length=5></DynamicList>
     }
 }
 
@@ -47,5 +50,58 @@ fn StaticList(length: u8) -> impl IntoView {
 
     view! {
         <ul>{counter_buttons}</ul>
+    }
+}
+
+#[component]
+fn DynamicList(initial_length: u8) -> impl IntoView {
+    let mut next_counter_id =initial_length;
+    let initial_counters = (0..initial_length)
+        .map(|id| (id, ArcRwSignal::new(id+1)))
+        .collect::<Vec<_>>();
+    let (counters, set_counters) = signal(initial_counters);
+    let add_counter = move |_| {
+        let sig = ArcRwSignal::new(next_counter_id + 1);
+        set_counters.update(move |counters| {
+            counters.push((next_counter_id, sig));
+        });
+        next_counter_id += 1;
+    };
+    
+    view! {
+        <div>
+            <button on:click=add_counter>
+                "Add counter"
+            </button>
+            <ul>
+                <For
+                    each=move || counters.get()
+                    key=|counter| counter.0
+                    children=move |(id, count)| {
+                        let count = RwSignal::from(count);
+                        view! {
+                            <li>
+                                <button
+                                    on:click=move |_| *count.write() += 1
+                                >
+                                    {count}
+                                </button>
+                                <button
+                                    on:click=move |_| {
+                                        set_counters
+                                            .write()
+                                            .retain(|(counter_id, _)| {
+                                                counter_id != &id
+                                            });
+                                    }
+                                >
+                                    "Remove"
+                                </button>
+                            </li>
+                        }
+                    }
+                />
+            </ul>
+        </div>
     }
 }
