88724dc834693356bc5e74e6055a28193caa5916
= <=>
2026-01-12 11:20:20 +0100

    feat: static list of buttons

--- src/main.rs
+++ src/main.rs
@@ -24,5 +24,28 @@ fn Header(pages: Vec<String>) -> impl IntoView {
                 </ul>
             </nav>
         </header>
+        <StaticList length=5></StaticList>
+    }
+}
+
+#[component]
+fn StaticList(length: u8) -> impl IntoView {
+    let counters = (1..=length).map(|id| RwSignal::new(id));
+    let counter_buttons = counters
+        .map(|count| {
+            view! {
+                <li>
+                    <button
+                        on:click=move |_| *count.write() += 1
+                    >
+                        {count}
+                    </button>
+                </li>
+            }
+        })
+        .collect_view();
+
+    view! {
+        <ul>{counter_buttons}</ul>
     }
 }
