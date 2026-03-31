ce1e0f85ca4bf5c4c8fd1d8719309828bbf254bb
stian <65687147+sti-and@users.noreply.github.com>
2026-03-31 16:14:24 +0200

    feat: body

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use leptos::prelude::*;
 fn main() {
     mount_to_body(|| {
         view! {
-          <h1>"JOSTILIM"</h1>
-          <p>"Det går et rykte om at.......👀"</p>
+          // <h1>"JOSTILIM"</h1>
+          // <p>"Det går et rykte om at.......👀"</p>
         }
     });
 } 
