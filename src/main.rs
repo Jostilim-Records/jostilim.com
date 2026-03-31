64f7c2e6c89dd3016879973afce3dffc5900da0b
Bruker <bruker@Mac.home>
2026-03-31 13:39:11 +0200

    fix(leptos): update imports for leptos 0.8

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use leptos::*;
+use leptos::prelude::*;
 
 fn main() {
     mount_to_body(|| {
         view! {
           <h1>"JOSTILIM"</h1>
           <p>"Det går et rykte om at.......👀"</p>
-        })
-}
+        }
+    });
+} 
